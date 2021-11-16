c2bf1c61fa9e123b4feed10868312094a1521d3b
romanver <romanver777@yandex.ru>
2021-11-16 14:59:00 +0300

    Updates

---
+++ precache-manifest.2996221d10940eb31fc3e4b5c0fd653d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0162e5832b462dfa38ff85a26ef7769",
+    "url": "/six-cities-react/index.html"
+  },
+  {
+    "revision": "cafe82683bd81650ea58",
+    "url": "/six-cities-react/static/css/main.10e7b50a.chunk.css"
+  },
+  {
+    "revision": "a092c1a395257184e9d7",
+    "url": "/six-cities-react/static/js/2.852dccf3.chunk.js"
+  },
+  {
+    "revision": "f4d2aba8d2992b1b26869bad9d322e4d",
+    "url": "/six-cities-react/static/js/2.852dccf3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cafe82683bd81650ea58",
+    "url": "/six-cities-react/static/js/main.ca1157b0.chunk.js"
+  },
+  {
+    "revision": "647db3b1dc382d3283da",
+    "url": "/six-cities-react/static/js/runtime-main.1c9777ef.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/six-cities-react/precache-manifest.dbcd296bf82ae1e6b968169bfaedefc0.js"
+  "/six-cities-react/precache-manifest.2996221d10940eb31fc3e4b5c0fd653d.js"
 );
 
 self.addEventListener('message', (event) => {
